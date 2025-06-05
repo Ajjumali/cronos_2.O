ededb35f75c7718d027cdeeb4f8000aa877fb37c
WEBOSPHERE\ajay.mali <ajay.mali@webosphere.in>
2025-06-05 15:09:27 +0530

    corn policy allow

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,33 @@ const nextConfig: NextConfig = {
     // Disable TypeScript type checking during production builds
     ignoreBuildErrors: true,
   },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'https://cronos-sand.vercel.app',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-Requested-With, Content-Type, Authorization',
+          },
+          {
+            key: 'Access-Control-Allow-Credentials',
+            value: 'true',
+          },
+        ],
+      },
+    ]
+  },
+  //below think added by me
   redirects: async () => {
     return [
       {
